e273fa1207188d84993548a8c83e5c9bd12e816c
ampuriraelizabeth <elizabethampurira@gmail.com>
2022-06-28 19:22:24 +0300

    js

--- index.js
+++ index.js
@@ -1,3 +1,67 @@
+for (var n=1; n<100; n++){
+    if  (n % 3==0 && n % 5==0){
+        console.log("fizzBuzz")}
+    else if (n % 5==0){
+        console.log("Buzz")}
+    else if (n % 3==0){
+        console.log("Fizz") }
+    else{
+        console.log(n)
+    }    
+  
+    }
+
+    // number2  
+    var sum =0
+for (var i=0; i<1000; i++){
+    if (i%3==0 && i%5==0){
+        sum+=i;
+    }
+} 
+console.log(sum); 
+
+// numb 3
+ 
+for(var x=0;x<21; x++){
+    if(x%2==0){
+    console.log(x,  "is even")}
+    else{
+        console.log(x,  "is odd")
+    }        
+    
+}
+// numb 4
+const  num = [-2, 4,-5, 6,0]
+let biggest_num=num[0]
+for (var m=0; m<num.length;m++){
+    if(num[m]>biggest_num){
+        biggest_num=num[m]
+       
+    } }
+console.log(biggest_num)
+
+// numb 5
+function largest(y,z){
+if (y>z){
+return y
+}
+else{
+return z
+}
+}
+console.log(largest(10,40))
+
+// numb 6
+function leapyear(year){
+if(year%4 && year%100 && year%400){
+return year + " is a leap year"
+}
+else{
+return year +  " is not a leap year"
+}
+}
+console.log(leapyear(2000))
+
 // 1. Write a JavaScript program which iterates the integers from 1 to 100. But for
 // multiples of three print "Fizz" instead of the number and for the multiples of five print
 // "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
